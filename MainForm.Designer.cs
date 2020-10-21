d9d73623ca6de9ec41de0edfa091b862907efe7d
Justin Steyn <justin_steyn@dialog.com.au>
2020-10-21 11:13:06 +1100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Challenge.Utilities.LogParser.Sample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.filepathTextBox = new System.Windows.Forms.TextBox();
+            this.parseButton = new System.Windows.Forms.Button();
+            this.uniqueTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addressListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.urlListView = new System.Windows.Forms.ListView();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.openLogFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(12, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse file";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filepathTextBox
+            // 
+            this.filepathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filepathTextBox.Location = new System.Drawing.Point(93, 12);
+            this.filepathTextBox.Name = "filepathTextBox";
+            this.filepathTextBox.Size = new System.Drawing.Size(311, 23);
+            this.filepathTextBox.TabIndex = 1;
+            // 
+            // parseButton
+            // 
+            this.parseButton.Location = new System.Drawing.Point(12, 41);
+            this.parseButton.Name = "parseButton";
+            this.parseButton.Size = new System.Drawing.Size(75, 23);
+            this.parseButton.TabIndex = 2;
+            this.parseButton.Text = "Parse file";
+            this.parseButton.UseVisualStyleBackColor = true;
+            this.parseButton.Click += new System.EventHandler(this.parseButton_Click);
+            // 
+            // uniqueTextBox
+            // 
+            this.uniqueTextBox.Location = new System.Drawing.Point(93, 42);
+            this.uniqueTextBox.Name = "uniqueTextBox";
+            this.uniqueTextBox.Size = new System.Drawing.Size(100, 23);
+            this.uniqueTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "unique IP addresses";
+            // 
+            // addressListView
+            // 
+            this.addressListView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.addressListView.HideSelection = false;
+            this.addressListView.Location = new System.Drawing.Point(12, 70);
+            this.addressListView.Name = "addressListView";
+            this.addressListView.Size = new System.Drawing.Size(392, 97);
+            this.addressListView.TabIndex = 5;
+            this.addressListView.UseCompatibleStateImageBehavior = false;
+            this.addressListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Top IP addresses";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Count";
+            // 
+            // urlListView
+            // 
+            this.urlListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.urlListView.HideSelection = false;
+            this.urlListView.Location = new System.Drawing.Point(12, 173);
+            this.urlListView.Name = "urlListView";
+            this.urlListView.Size = new System.Drawing.Size(392, 97);
+            this.urlListView.TabIndex = 6;
+            this.urlListView.UseCompatibleStateImageBehavior = false;
+            this.urlListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Top URLs";
+            this.columnHeader3.Width = 300;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Count";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 282);
+            this.Controls.Add(this.urlListView);
+            this.Controls.Add(this.addressListView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uniqueTextBox);
+            this.Controls.Add(this.parseButton);
+            this.Controls.Add(this.filepathTextBox);
+            this.Controls.Add(this.browseButton);
+            this.Name = "MainForm";
+            this.Text = "Coding Challenge Sample Application";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox filepathTextBox;
+        private System.Windows.Forms.Button parseButton;
+        private System.Windows.Forms.TextBox uniqueTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView addressListView;
+        private System.Windows.Forms.ListView urlListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.OpenFileDialog openLogFileDialog;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Challenge.Utilities.LogParser;
+using Challenge.Utilities.LogParser.Models;
+
+namespace Challenge.Utilities.LogParser.Sample
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openLogFileDialog.ShowDialog();
+
+            if ( result == DialogResult.OK)
+            {
+                filepathTextBox.Text = openLogFileDialog.FileName;
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void parseButton_Click(object sender, EventArgs e)
+        {
+            string LogPath = filepathTextBox.Text;
+            LogParser lp = new LogParser();
+
+            lp.ParseLogFile(LogPath);
+
+            uniqueTextBox.Text = lp.TotalUniqueIpAddresses().ToString();
+
+
+            List<LogItemTotal> totalAddressList = lp.GetTopIpAddresses(5);
+
+            foreach (LogItemTotal item in totalAddressList)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = item.LogItemGroup;
+                lvi.SubItems.Add(item.TotalGroup.ToString());
+                addressListView.Items.Add(lvi);
+            }
+
+
+            List<LogItemTotal> totalUrlList = lp.GetTopUrls(3);
+
+            foreach (LogItemTotal item in totalUrlList)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = item.LogItemGroup;
+                lvi.SubItems.Add(item.TotalGroup.ToString());
+                urlListView.Items.Add(lvi);
+            }
+
+        }
+    }
+}
